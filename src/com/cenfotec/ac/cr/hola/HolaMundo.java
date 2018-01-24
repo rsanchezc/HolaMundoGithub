aa6e0463da074e4876e032a4ae5fa34e8dfc9bfe
Rogelio <rsanchezc@ucenfotec.ac.cr>
2018-01-24 17:15:25 -0600

    Italiano = Ciao Mondo

--- src/com/cenfotec/ac/cr/hola/HolaMundo.java
+++ src/com/cenfotec/ac/cr/hola/HolaMundo.java
@@ -10,8 +10,8 @@ package com.cenfotec.ac.cr.hola;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Aleman");
-		System.out.println("Hallo Welt");
+		System.out.println("Italiano");
+		System.out.println("Ciao Mondo");
 	}
 }
 	
