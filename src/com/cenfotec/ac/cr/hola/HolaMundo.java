232784a69f19e0141b5115b971d66f4f5110b64a
Rogelio <rsanchezc@ucenfotec.ac.cr>
2018-01-24 17:12:19 -0600

    Español = Hola Mundo

---
+++ src/com/cenfotec/ac/cr/hola/HolaMundo.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author Rogelio Sánchez Chow
+ *
+ */
+package com.cenfotec.ac.cr.hola;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Español");
+		System.out.println("Hola Mundo");
+	}
+}
+	
