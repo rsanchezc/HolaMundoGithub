e640696a9f907f38becd55fda448f7d0991f8c04
Rogelio <rsanchezc@ucenfotec.ac.cr>
2018-01-24 17:13:53 -0600

    Aleman =  Hallo Welt

--- src/com/cenfotec/ac/cr/hola/HolaMundo.java
+++ src/com/cenfotec/ac/cr/hola/HolaMundo.java
@@ -10,8 +10,8 @@ package com.cenfotec.ac.cr.hola;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Español");
-		System.out.println("Hola Mundo");
+		System.out.println("Aleman");
+		System.out.println("Hallo Welt");
 	}
 }
 	
